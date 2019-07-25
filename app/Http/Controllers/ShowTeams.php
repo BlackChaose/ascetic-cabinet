2abb3acb33129a5e64334f01064555df002667f9
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-07-25 09:31:52 +0300

    second commit. test work with node

--- app/Http/Controllers/ShowTeams.php
+++ app/Http/Controllers/ShowTeams.php
@@ -5,30 +5,42 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 //use GuzzleHttp;
-
+//FIXME - разобраться с парсингом! авторизацией на сторонних ресурсах!
 
 class ShowTeams extends Controller
 {
-	private $username = 'nikita.s.kalitin@gmail.com';
-	private $password = 'Saturn33!';
-	private $additionalHeaders="";
-	//private $url= 'https://xn--b1aaqfxbbhefb3bya5f.xn--p1ai/lk/';
-	private $url = 'http://httpbin.org/cookies';
-	private $headers_request="";
+	//~ private $username = 'nikita.s.kalitin@gmail.com';
+	//~ private $password = 'Saturn33!';
+	//~ private $additionalHeaders="";
+	//private $url= 'https://xn--b1aaqfxbbhefb3bya5f.xn--p1ai/hackathon/users/';
+	/* https://цифровойпрорыв.рф/hackathon/users/   */
+	//private $url = 'http://httpbin.org/cookies';
+	private $url= 'http://kalitinns.mno.extech.ru/admin/login';
+	private $tst_url = 'http://kalitinns.mno.extech.ru/admin/organizations';
+	
     
     private function parseCP(){
 		$client = new \GuzzleHttp\Client(['cookies' => true]);
 		$jar = new \GuzzleHttp\Cookie\CookieJar;
 		
-		$response = $client->request('GET',$this->url,["cookies"=>$jar]);
+		$response = $client->request('GET', $this->url,['cookies'=>$jar]);
 		
-		//$response = json_decode($response->getBody(), true);
+		$token=$response->getHeaders()["Set-Cookie"][0];
+		
+		//~ $response2 = $client->post($this->url,['headers'=>['X-CSRF-TOKEN'=>$token]],['form_params' => [
+        //~ '_token' => $token,
+        //~ 'email' => 'nikita.s.kalitin@gmail.com',
+        //~ 'password' => '6quedd0v']
+        //~ ]);
+
+		//$response = $client->request('GET', $this->tst_url,['cookies'=>$jar]);
+			
 		return $response;
 	}
 	public function showTeams(){
 		$res = $this->parseCP();
 		if(!empty($res)){
-		print_r($this->parseCP());
+		print$this->parseCP()->getBody();
 		}
 		else{
 			print_r(json_encode(["stats"=>"empty request"]));

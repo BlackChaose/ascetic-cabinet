f2bf24b4d446f0db53562c3c3fd4430104a71ee2
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-08-12 17:50:47 +0300

    save commit

--- public/js/app.js
+++ public/js/app.js
@@ -52075,8 +52075,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/nik/VMs/MSrv2/blackchaose.ascetic-cabinet/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/nik/VMs/MSrv2/blackchaose.ascetic-cabinet/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/nik/DEV/my/blackchaose.ascetic-cabinet/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/nik/DEV/my/blackchaose.ascetic-cabinet/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
